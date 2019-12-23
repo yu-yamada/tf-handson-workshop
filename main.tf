d6ad5936062344a2e7b5b8dbee21378e3c169bba
yu-yamada <yu_yamada@r.recruit.co.jp>
2019-12-23 16:50:03 +0900

    second commit

--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
 terraform {
-required_version = "~> 0.12"
+  required_version = "~> 0.12"
+}
+
+provider "google" {
+    credentials = var.gcp_key
+    project     = var.project
+    region      = var.region
+}
+
+resource "google_compute_instance" "vm_instance" {
+    name = "terraform-instance-${count.index}-yu"
+    count = var.hello_tf_instance_count
+    machine_type = var.machine_type
+    zone = "asia-northeast1-a"
+    boot_disk {
+        initialize_params {
+            image = var.image
+        }
+    }
+
+    network_interface {
+        # A default network is created for all GCP projects
+        network       = "default"
+        access_config {
+        }
+    }
 }
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "gcp_key" {}
+variable "machine_type" {}
+variable "hello_tf_instance_count" {
+    default = 1
+}
+variable "region" {
+    default = "asia-northeast1"
+}
+variable "project" {}
+variable "image" {}
