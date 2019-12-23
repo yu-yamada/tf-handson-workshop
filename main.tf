ab28afbcc773ca99be9dfad8ec299a1488835186
yu-yamada <yu_yamada@r.recruit.co.jp>
2019-12-23 17:27:47 +0900

    hoge

--- main.tf
+++ main.tf
@@ -3,26 +3,30 @@ terraform {
 }
 
 provider "google" {
-    credentials = var.gcp_key
-    project     = var.project
-    region      = var.region
+  credentials = var.gcp_key
+  project     = var.project
+  region      = var.region
 }
 
 resource "google_compute_instance" "vm_instance" {
-    name = "terraform-instance-${count.index}-yu1"
-    count = var.hello_tf_instance_count
-    machine_type = var.machine_type
-    zone = "asia-northeast1-a"
-    boot_disk {
-        initialize_params {
-            image = var.image
-        }
+  name = "terraform-instance-${count.index}"
+  machine_type = var.machine_type
+  count = var.hello_tf_instance_count
+  zone = asia-northeast1-a
+  labels = {
+    owner = "kabu",
+    ttl = "100"
+  }
+  boot_disk {
+    initialize_params {
+      image = var.image
     }
+  }
 
-    network_interface {
-        # A default network is created for all GCP projects
-        network       = "default"
-        access_config {
-        }
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config {
     }
+  }
 }
